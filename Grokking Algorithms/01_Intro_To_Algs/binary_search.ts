5fdf91fcdf9dec07bafdebe6094cfba4ee6c7169
steven <synopsikk@gmail.com>
2026-07-09 03:55:46 -0700

    Moved testing logic to main

--- Grokking Algorithms/01_Intro_To_Algs/binary_search.ts
+++ Grokking Algorithms/01_Intro_To_Algs/binary_search.ts
@@ -1,12 +1,9 @@
-import { peopleList } from "../data/people";
-function binarySearch(arr: number[], item: number): number {
+export function binarySearch<T>(arr: T[], item: T): number {
     let low: number = 0;
     let high: number = arr.length - 1;
-
     while (low <= high) {
         const mid = Math.floor((low + high) / 2);
         const guess = arr[mid];
-
         if (guess === item) {
             return mid;
         } else if (guess > item) {
@@ -15,15 +12,8 @@ function binarySearch(arr: number[], item: number): number {
             low = mid + 1;
         }
     }
-
     return -1; // Something failed if this runs
 }
 
-const myList = [1, 3, 5, 7, 9];
-
-console.log(binarySearch(myList, 9));
-console.log(binarySearch(myList, 6));
-
-console.log(binarySearch(peopleList, "Steven Baar"))
 
 
