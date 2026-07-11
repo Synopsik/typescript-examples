fe069ae7856bfd9fc4513e075e74f15a2be75c7b
Steven <synopsik@proton.me>
2026-07-11 03:32:44 -0700

    Created BFS implementation, now onto DFS

--- src/fileTree.ts
+++ src/fileTree.ts
@@ -1,6 +1,43 @@
+import * as fs from "fs";
+import * as path from "path";
+import {Queue} from "../util/queue";
+import { quickSort } from "../util/algorithms/quickSort";
 
 
-export function walkDir(dir: string) {
+export function walkDirBFS(dir: string): Record<string, string[]> {
+    const queue = new Queue<string>();
+    const dirTree: Record<string, string[]> = {}
+    queue.enqueue([dir]);
+    dirTree[dir] = [];
+    while (!queue.isEmpty) {
+        const dir = queue.dequeue();
+        if (dir) {
+            for (const file of quickSort(fs.readdirSync(dir))) {
+                const fullPath = path.join(dir, file)
+                const currFiles = dirTree[dir];
+                if (fs.statSync(fullPath).isFile()) {
+                    dirTree[dir] = [...(currFiles ?? []), file];
+                    // console.log(file)
+                } else {
+                    dirTree[dir] = [...(currFiles ?? []), fullPath]
+                    dirTree[fullPath] = [];
+                    queue.enqueue([fullPath])
+                }
+            }
+        }
+    }
+    return dirTree;
+}
+
+export function walkDirDFS(dir: string): Record<string, string[]> {
+
+}
 
 
+
+
+if (import.meta.main) {
+    console.log("\nFiles:")
+    const fileTree = walkDirBFS("..\\util");
+    console.log(fileTree);
 }
