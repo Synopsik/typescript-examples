7c7ce967522e5660190a1553e03288326bd111d8
Steven <synopsik@proton.me>
2026-07-11 03:59:05 -0700

    Added DFS walkDir function

--- src/fileTree.ts
+++ src/fileTree.ts
@@ -8,18 +8,17 @@ export function walkDirBFS(dir: string): Record<string, string[]> {
     const queue = new Queue<string>();
     const dirTree: Record<string, string[]> = {}
     queue.enqueue([dir]);
-    dirTree[dir] = [];
     while (!queue.isEmpty) {
         const dir = queue.dequeue();
         if (dir) {
             for (const file of quickSort(fs.readdirSync(dir))) {
                 const fullPath = path.join(dir, file)
-                const currFiles = dirTree[dir];
+                const currFiles = dirTree[dir] ?? [];
                 if (fs.statSync(fullPath).isFile()) {
-                    dirTree[dir] = [...(currFiles ?? []), file];
+                    dirTree[dir] = [...currFiles, file];
                     // console.log(file)
                 } else {
-                    dirTree[dir] = [...(currFiles ?? []), fullPath]
+                    dirTree[dir] = [...currFiles, fullPath]
                     dirTree[fullPath] = [];
                     queue.enqueue([fullPath])
                 }
@@ -29,8 +28,21 @@ export function walkDirBFS(dir: string): Record<string, string[]> {
     return dirTree;
 }
 
-export function walkDirDFS(dir: string): Record<string, string[]> {
 
+export function walkDirDFS(dir: string): Record<string, string[]> {
+    let dirTree: Record<string, string[]> = {};
+    for (const file of quickSort(fs.readdirSync(dir))) {
+        const fullPath = path.join(dir, file);
+        const currFiles = dirTree[dir] ?? [];
+        if (fs.statSync(fullPath).isFile()) {
+            dirTree[dir] = [...(currFiles), file];
+        } else {
+            dirTree[dir] = [...currFiles, fullPath];
+            const subDirRecords = walkDirDFS(fullPath);
+            dirTree = {...dirTree, ...subDirRecords};
+        }
+    }
+    return dirTree;
 }
 
 
