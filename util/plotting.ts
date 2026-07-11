ced24120f95f666c23cc5d30922c72526caadcb3
Steven <synopsik@proton.me>
2026-07-10 23:14:15 -0700

    Benchmarking code now has dedicated folder and files

--- util/plotting.ts
+++ util/plotting.ts
@@ -16,29 +16,3 @@ export function plotData(
     plot(data, layout);
 }
 
-
-export function averageCase<A extends unknown[], R>(
-    func: (...args: A) => R,
-    createArgs: (size: number) => A,
-    loops: number = 1_000_000,
-) {
-    const inputSizes: number[] = [];
-    const averageTimes: number[] = [];
-    const averagingLoops: number = 100;
-    let xNumber: number = 0;
-
-    for (let arraySize = 100; arraySize <= loops; arraySize *= 10) {
-        const args = createArgs(arraySize);
-
-        const start = performance.now();
-        for (let i = 0; i < averagingLoops; i++) {
-            func(...args);
-        }
-        const totalTime = performance.now() - start;
-
-        inputSizes.push(xNumber += 1);
-        averageTimes.push(totalTime / averagingLoops);
-    }
-
-    plotData(inputSizes, averageTimes);
-}
