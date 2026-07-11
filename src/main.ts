1c92cf968670750191396b53bd6a6664ea9daff9
Steven <synopsik@proton.me>
2026-07-11 02:06:41 -0700

    Added interactive CLI for running examples

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
+import * as readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
 import { findMangoSeller } from "./mangoSellers";
 
+async function main() {
+    const rl = readline.createInterface({input, output});
+    let answer = "";
 
-findMangoSeller();
+    while (answer !== "2") {
+        answer = await rl.question(
+            "What do you want to run first?\n" +
+            "1. Find Mango Seller\n" +
+            "2. Quit\n"
+        );
+        if (answer === "1") findMangoSeller();
+        console.log();
+    }
+    rl.close();
+}
+
+if (import.meta.main) {
+    main();
+}
